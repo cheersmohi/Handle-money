46e2de4b9999491115a93e4d6dc6a3495aa84d97
Mohiuddin MD Arif <mohiuddinarif@Mohiuddins-MacBook-Pro.local>
2023-11-17 01:45:40 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
+
+function getInputValue(inputid) {
+
+    const input = document.getElementById(inputid);
+    const inputAmountText = input.value;
+    const amountValue = parseFloat(inputAmountText);
+    input.value = '';
+    return amountValue;
+}
+
+
+
+
+
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
 
-    const incomeInput = document.getElementById('income-field');
-    const incomeAmountString = incomeInput.value;
-    const incomeTextAmount = parseFloat(incomeAmountString);
-    incomeInput.value = '';
+    // const incomeInput = document.getElementById('income-input');
+    // const incomeAmountString = incomeInput.value;
+    // const incomeTextAmount = parseFloat(incomeAmountString);
+    const incomeTextAmount = getInputValue('income-input');
+    // incomeInput.value = '';
 
 
     const foodField = document.getElementById('food-input');
@@ -35,8 +51,31 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const restTotalAmount = incomeTextAmount - totalExpenses;
     balanceField.innerText = restTotalAmount;
 
+
 })
 
 
+
+
+document.getElementById('save-btn').addEventListener('click', function () {
+
+    // const saveInputField = document.getElementById('save-input');
+    // const saveInputAmount = saveInputField.value;
+    // const saveInputText = parseFloat(saveInputAmount);
+
+    const saveInputText = getInputValue('save-input')
+
+    const totalSave = incomeTextAmount / 10;
+    saveInputField.innerText = totalSave;
+
+
+    const savingAmount = document.getElementById('saving-amount');
+    const savingAmountString = savingAmount.innerText;
+    const savingText = parseFloat(savingAmountString);
+
+
+
+
+})
 
 
